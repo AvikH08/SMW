ec3429fb0250ddbf6d6f9da5ce38d5850af14d84
Avik Halder <avikhalderlegion@gmail.com>
2022-12-23 18:03:33 +0530

    setup static files access

--- routes/index.js
+++ routes/index.js
@@ -4,7 +4,11 @@ const homeController = require('../controllers/home_controller.js')
 
 router.get('/', homeController.home)
 
+router.use('/users', require('./users.js'));
 
 console.log('router loaded')
+
+//for any further routes, access from home
+///router.use('/routerName', require('./routerfile'))
 
 module.exports = router;
